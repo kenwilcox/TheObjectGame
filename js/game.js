4561b4c657337d3b442546d1ca50d41c60226647
iAutin <autin.igor@gmail.com>
2016-01-28 09:07:53 -0700

    added igor's portion and instructions for chris.

--- js/game.js
+++ js/game.js
@@ -1,15 +1,24 @@
 var health = 100;
+var pcname 
 
 function slap(){
       health -= 1
     update(health);
-  
 }
 
+function kick(){
+      health -= 10
+    update(health);
+}
 
 function update(){
-    
     document.getElementById("pchp").innerText = health.toString();
-    
 }
 update()
+
+
+
+function test(){
+    var userInput = document.getElementById("userInput").value;
+    document.getElementById("pcname").innerHTML = userInput;
+}
