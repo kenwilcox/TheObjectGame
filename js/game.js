8820aa84d1facbef203db7edbd2ec51aff629cee
kilowatt11 <kilowatt11@cableone.net>
2016-01-27 21:11:19 -0700

    update to slap and update functions

--- js/game.js
+++ js/game.js
@@ -1,6 +1,15 @@
 var health = 100;
 
 function slap(){
-    health -= 1;
-    alert(health);   
+      health -= 1
+    update(health);
+  
 }
+
+
+function update(){
+    
+    document.getElementById("pchp").innerText = health.toString();
+    
+}
+update()
