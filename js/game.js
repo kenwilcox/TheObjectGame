b5feeb09b628dc97930eface3072799a72bf0348
Ken Wilcox <wilcoxk@medamerica.com>
2016-01-29 08:30:50 -0700

    For Kurt

--- js/game.js
+++ js/game.js
@@ -1,8 +1,23 @@
+var Item = function(name, modifier, description){
+    this.name = name;
+    this.modifier = modifier;
+    this.description = description;
+    this.draw = function(){
+     //...
+    }
+}
+var items = {
+    shield: new Item("Shield",0.3,"This is an awesome shield!"),
+    sword :new Item("Sword",0.5,"Amazing Sword of Epic Death"),
+    helmet: new Item("Helmet",0.2,"Solid Head Protection!"),
+    breastplate: new Item("Breastplate",0.4,"Big, ugly and safe"),
+}
+
 var Player = function(){
     this.health = 100;
     this.pcname ="";
     this.hit = 0;
-    
+    this.items = [items.shield, items.helmet];    
 }
 var player = new Player();
 
@@ -27,8 +42,6 @@ function punch() {
     
 }
 
-
-
 function test() {
     var userInput = document.getElementById("userInput").value;
     document.getElementById("pcname").innerHTML = userInput;
@@ -37,20 +50,6 @@ function test() {
 function update() {
     document.getElementById("pchp").innerText = player.health.toString();
     document.getElementById("pchit").innerText = player.hit.toString();
+    alert(JSON.stringify(player.items));
 }
-update()
-
-var Item = function(name, modifier, description){
-    this.name = name;
-    this.modifier = modifier;
-    this.description = description;
-    this.draw = function(){
-     //...
-    }
-}
-var items = {
-    shield: new Item("Shield",0.3,"This is an awesome shield!"),
-    sword :new Item("Sword",0.5,"Amazing Sword of Epic Death"),
-    helmet: new Item("Helmet",0.2,"Solid Head Protection!"),
-    breastplate: new Item("Breastplate",0.4,"Big, ugly and safe"),
-}
+update();
