96ae5cc7408dfe624ca1f85301fa05f7c96685e0
kilowatt11 <kilowatt11@cableone.net>
2016-01-28 22:06:40 -0700

    updated functions per Ken. Created player object. Added a constructor and created items.

--- js/game.js
+++ js/game.js
@@ -1,36 +1,59 @@
-var health = 100;
-var pcname ="";
-var hit = 0;
+var Player = function(){
+    this.health = 100;
+    this.pcname ="";
+    this.hit = 0;
+}
+var player = new Player();
+//var health = 100;
+//var pcname ="";
+//var hit = 0;
 
-function slap(){
-      health -= 1
-      hit += 1
+function slap() {
+    player.health -= 1;
+    player.hit += 1;
     update();
 }
 
-function kick(){
-    health -= 10
-    hit += 1
+function kick() {
+    player.health -= 10;
+    player.hit += 1;
     update();
-    //update(health);
+    
 }
 
-function punch(){
-    health -= 5
-    hit += 1
+function punch() {
+    player.health -= 5;
+    player.hit += 1;
     update();
-    //update(health);
+    
 }
 
 
 
-function test(){
+function test() {
     var userInput = document.getElementById("userInput").value;
     document.getElementById("pcname").innerHTML = userInput;
 }
 
-function update(){
-    document.getElementById("pchp").innerText = health.toString();
-    document.getElementById("pchit").innerText = hit.toString();
+function update() {
+    document.getElementById("pchp").innerText = player.health.toString();
+    document.getElementById("pchit").innerText = player.hit.toString();
 }
 update()
+
+var Item = function(name, modifier, description){
+    this.name = name;
+    this.modifier = modifier;
+    this.description = description;
+    this.draw = function(){
+     //...
+    }
+}
+var items = {
+    shield:new Item("Shield",0.3,"This is an awesome shield!"),
+    sword:new Item("Sword",0.5,"Amazing Sword of Epic Death"),
+    crossbow:new Item("Crossbow",0.2,"Bang, you're dead!"),
+    mace:new Item("Mace",0.3,"Big Stick of Blunt Trauma!"),
+    helmet:new Item("Helmet",0.2,"Solid Head Protection!"),
+    breastplate:new Item("Breastplate",0.4,"Big, ugly and safe"),
+}
